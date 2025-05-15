c22d8f8e4e5042847d342f783e55d00fa4b458c5
Alexandre Chs <oliuuum@gmail.com>
2025-05-15 07:24:33 +0200

    feat: template email forgot password

--- app/auth/events/index.ts
+++ app/auth/events/index.ts
@@ -3,14 +3,5 @@ import emitter from '@adonisjs/core/services/emitter'
 import mail from '@adonisjs/mail/services/main'
 
 emitter.on('auth:forgot-password', async function (user) {
-  // here you can send the email to the user
-
-  await EmailService.sendEmail(user.email, 'Forgot Password')
-  // await mail.send((message) => {
-  //   message
-  //     .to(user.email)
-  //     .from('test@gmail.com')
-  //     .subject('Forgot Password')
-  //     .html('<h1>Click here to reset your password</h1>')
-  // })
+  await EmailService.sendEmail(user.email, 'Forgot Password', 'forgot_password_mjml')
 })
--- app/services/email_service.ts
+++ app/services/email_service.ts
@@ -1,3 +1,4 @@
+import env from '#start/env'
 import User from '#users/models/user'
 import mail from '@adonisjs/mail/services/main'
 
@@ -8,12 +9,22 @@ type EmailData = ResetPasswordData | WelcomeData
 
 export default class EmailService {
   static from: string = 'test@gmail.com'
+  static companyName: string = `${env.get('COMPANY_NAME')}`
+  static companyLogoUrl: string = `${env.get('COMPANY_LOGO_URL')}`
+  static year: number = new Date().getFullYear()
 
-  static async sendEmail(to: string, subject: string, data?: WelcomeData | ResetPasswordData) {
+  static async sendEmail(
+    to: string,
+    subject: string,
+    view: string,
+    data?: WelcomeData | ResetPasswordData
+  ) {
     await mail.send((message) => {
-      message.to(to).from(this.from).subject(subject).htmlView('emails/verify_email_mjml', {
-        username: 'test username lol',
-        verifyUrl: 'https://example.com/verify',
+      message.to(to).from(this.from).subject(subject).htmlView(`emails/${view}`, {
+        resetUrl: 'https://example.com/verify',
+        companyLogoUrl: this.companyLogoUrl,
+        projectName: this.companyName,
+        year: this.year,
       })
     })
   }
